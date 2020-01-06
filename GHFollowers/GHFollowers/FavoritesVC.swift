68a6d1c9e6d8c8c3c63678a5d2bc232eb3157296
Eugene Berezin <berezin.nlp@gmail.com>
2020-01-05 20:45:36 -0800

    ready

---
+++ GHFollowers/GHFollowers/FavoritesVC.swift
@@ -0,0 +1,31 @@
+//
+//  FavoritesVC.swift
+//  GHFollowers
+//
+//  Created by Eugene Berezin on 1/5/20.
+//  Copyright © 2020 Eugene Berezin. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGray4
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GHFollowers/GHFollowers/SearchViC.swift
@@ -0,0 +1,31 @@
+//
+//  SearchViC.swift
+//  GHFollowers
+//
+//  Created by Eugene Berezin on 1/5/20.
+//  Copyright © 2020 Eugene Berezin. All rights reserved.
+//
+
+import UIKit
+
+class SearchViC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
